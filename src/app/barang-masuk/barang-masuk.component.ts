d803fbdcba5f4efc76c2c2e0d36880b5a4c9609f
Roid <132744222+TWCS911@users.noreply.github.com>
2025-01-13 00:01:52 +0700

    new

--- src/app/barang-masuk/barang-masuk.component.ts
+++ src/app/barang-masuk/barang-masuk.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { TransaksiService } from '../services/transaksi.service';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { BarangService } from '../services/barang.service';
-import { Transaksi } from '../models/transaksi.model';
+import { TransaksiService } from '../services/transaksi.service';
 import { Barang } from '../models/barang.model';
+import { Transaksi } from '../models/transaksi.model';
 import Swal from 'sweetalert2';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-barang-masuk',
@@ -11,19 +13,23 @@ import Swal from 'sweetalert2';
   templateUrl: './barang-masuk.component.html',
   styleUrls: ['./barang-masuk.component.css']
 })
-export class BarangMasukComponent implements OnInit {
+export class BarangMasukComponent implements OnInit, OnDestroy {
   barangList: Barang[] = []; // Daftar barang
   transaksiList: Transaksi[] = []; // Daftar transaksi barang masuk
-  filteredBarangMasukList: any[] = []; // Barang yang sudah difilter berdasarkan pencarian
+  filteredBarangMasukList: Transaksi[] = []; // Barang yang sudah difilter berdasarkan pencarian
   selectedBarangId: string = ''; // ID barang yang dipilih
   jumlah: number = 0; // Jumlah barang yang masuk
   keterangan: string = ''; // Keterangan barang masuk
   searchQuery: string = ''; // Query pencarian untuk barang masuk
   p: number = 1; // Halaman untuk pagination (default 1)
 
+  private getBarangSub: Subscription = new Subscription();
+  private getTransaksiSub: Subscription = new Subscription();
+
   constructor(
+    private barangService: BarangService,
     private transaksiService: TransaksiService,
-    private barangService: BarangService
+    @Inject(PLATFORM_ID) private platformId: any
   ) {}
 
   ngOnInit(): void {
@@ -31,22 +37,40 @@ export class BarangMasukComponent implements OnInit {
     this.loadTransaksiList(); // Memuat transaksi barang masuk
   }
 
+  ngOnDestroy(): void {
+    // Unsubscribe untuk mencegah memory leaks
+    this.getBarangSub.unsubscribe();
+    this.getTransaksiSub.unsubscribe();
+  }
+
   // Memuat Daftar Barang dari BarangService
   loadBarangList(): void {
-    this.barangService.getBarang();  // Meminta BarangService untuk mengambil data barang
-    this.barangService.getBarangListener().subscribe(
-      (data) => {
-        this.barangList = data;  // Menyimpan data barang yang diterima
-      },
-      (error) => {
-        console.error('Error loading barang list!', error);
+    if (isPlatformBrowser(this.platformId)) {
+      // Periksa apakah data barang sudah ada di localStorage
+      const storedBarangList = localStorage.getItem('barangList');
+      if (storedBarangList) {
+        this.barangList = JSON.parse(storedBarangList);
+      } else {
+        // Jika tidak ada, ambil data dari API
+        this.getBarangSub = this.barangService.getBarangListener().subscribe(
+          (data) => {
+            this.barangList = data;  // Menyimpan data barang yang diterima
+            localStorage.setItem('barangList', JSON.stringify(data));  // Simpan ke localStorage
+          },
+          (error) => {
+            console.error('Error loading barang list!', error);
+          }
+        );
+        this.barangService.getBarang(); // Panggil API untuk mengambil data barang
       }
-    );
+    } else {
+      console.warn('localStorage is not available in this environment');
+    }
   }
 
   // Memuat Daftar Transaksi Barang Masuk
   loadTransaksiList(): void {
-    this.transaksiService.getBarangMasuk().subscribe(
+    this.getTransaksiSub = this.transaksiService.getBarangMasuk().subscribe(
       (data: Transaksi[]) => {
         this.transaksiList = data;  // Menyimpan data transaksi yang diterima
         this.filteredBarangMasukList = this.transaksiList; // Menampilkan seluruh transaksi sebelum pencarian
@@ -69,14 +93,11 @@ export class BarangMasukComponent implements OnInit {
           ? tanggalMasuk.toISOString().split('T')[0] // Format sebagai YYYY-MM-DD
           : ''; // Jika tanggal tidak valid, berikan string kosong
   
-        // Cek apakah query cocok dengan nama, merk, jenis, tanggalMasuk, atau keterangan
-        const dateQuery = this.isValidDate(query) ? this.formatDate(query) : ''; // Konversi tanggal dari input query jika valid
-  
         return (
           transaksi.barang.nama.toLowerCase().includes(query) ||
           transaksi.barang.merk.toLowerCase().includes(query) ||
           transaksi.barang.jenis.toLowerCase().includes(query) ||
-          formattedTanggalMasuk.includes(dateQuery) || // Pencarian berdasarkan tanggal
+          formattedTanggalMasuk.includes(query) || // Pencarian berdasarkan tanggal
           (transaksi.keterangan && transaksi.keterangan.toLowerCase().includes(query)) // Pencarian berdasarkan keterangan
         );
       });
@@ -84,26 +105,6 @@ export class BarangMasukComponent implements OnInit {
       this.filteredBarangMasukList = this.transaksiList; // Tampilkan seluruh transaksi jika pencarian kosong
     }
   }
-  
-  // Fungsi untuk memvalidasi dan format tanggal
-  isValidDate(date: string): boolean {
-    const regex = /^(0[1-9]|1[0-9]|2[0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/; // Format DD/MM/YYYY
-    return regex.test(date);
-  }
-  
-  formatDate(date: string): string {
-    const parts = date.split('/');
-    // Jika format tanggal valid, rubah ke YYYY-MM-DD
-    if (parts.length === 3) {
-      const day = parts[0].padStart(2, '0');
-      const month = parts[1].padStart(2, '0');
-      const year = parts[2];
-      return `${year}-${month}-${day}`; // Format YYYY-MM-DD
-    }
-    return '';
-  }
-  
-  
 
   // Fungsi untuk menambah barang masuk
   tambahBarangMasuk(): void {
@@ -124,83 +125,67 @@ export class BarangMasukComponent implements OnInit {
   }
 
   tampilkanFormTambah(): void {
-  // Menampilkan modal SweetAlert2 dengan form input pencarian barang
-  Swal.fire({
-    title: 'Tambah Barang Masuk',
-    html: `
-      <div class="form-group mb-3">
-        <!-- Select untuk memilih barang -->
-        <select id="barangSelect" class="form-control" style="margin-top: 10px;">
-          <option value="">-- Pilih Barang --</option>
-          ${this.barangList.map(barang => `
-            <option value="${barang._id}">${barang.nama} - ${barang.merk}</option>
-          `).join('')}
-        </select>
-      </div>
-      <div class="form-group mb-3">
-        <input type="number" id="jumlahInput" class="form-control" min="1" placeholder="Jumlah barang">
-      </div>
-      <div class="form-group">
-        <input type="text" id="keteranganInput" class="form-control" placeholder="Masukkan keterangan">
-      </div>
-    `,
-    showCancelButton: true,
-    confirmButtonText: 'Tambah',
-    cancelButtonText: 'Batal',
-    preConfirm: () => {
-      const selectedBarangId = (document.getElementById('barangSelect') as HTMLSelectElement).value;
-      const jumlah = (document.getElementById('jumlahInput') as HTMLInputElement).value;
-      const keterangan = (document.getElementById('keteranganInput') as HTMLInputElement).value;
+    // Menampilkan modal SweetAlert2 dengan form input pencarian barang
+    Swal.fire({
+      title: 'Tambah Barang Masuk',
+      html: `
+        <div class="form-group mb-3">
+          <!-- Select untuk memilih barang -->
+          <select id="barangSelect" class="form-control" style="margin-top: 10px;">
+            <option value="">-- Pilih Barang --</option>
+            ${this.barangList.map(barang => `
+              <option value="${barang._id}">${barang.nama} - ${barang.merk}</option>
+            `).join('')}
+          </select>
+        </div>
+        <div class="form-group mb-3">
+          <input type="number" id="jumlahInput" class="form-control" min="1" placeholder="Jumlah barang">
+        </div>
+        <div class="form-group">
+          <input type="text" id="keteranganInput" class="form-control" placeholder="Masukkan keterangan">
+        </div>
+      `,
+      showCancelButton: true,
+      confirmButtonText: 'Tambah',
+      cancelButtonText: 'Batal',
+      preConfirm: () => {
+        const selectedBarangId = (document.getElementById('barangSelect') as HTMLSelectElement).value;
+        const jumlah = (document.getElementById('jumlahInput') as HTMLInputElement).value;
+        const keterangan = (document.getElementById('keteranganInput') as HTMLInputElement).value;
 
-      if (!selectedBarangId || !jumlah || parseInt(jumlah) <= 0) {
-        Swal.showValidationMessage('Harap pilih barang dan masukkan jumlah yang valid!');
-        return false; // Tidak melanjutkan proses jika data tidak valid
-      }
-
-      return { selectedBarangId, jumlah: parseInt(jumlah), keterangan };
-    }
-  }).then((result) => {
-    if (result.isConfirmed) {
-      const { selectedBarangId, jumlah, keterangan } = result.value;
-      // Menambahkan barang masuk melalui transaksiService
-      this.transaksiService.tambahBarangMasuk(selectedBarangId, jumlah, keterangan).subscribe(
-        (response) => {
-          Swal.fire({
-            title: 'Berhasil!',
-            text: 'Barang masuk berhasil ditambahkan.',
-            icon: 'success',
-            confirmButtonText: 'OK'
-          }).then(() => {
-            // Refresh halaman untuk memuat data baru
-            window.location.reload();
-          });
-        },
-        (error) => {
-          Swal.fire('Gagal!', 'Terjadi kesalahan saat menambahkan barang masuk.', 'error');
-          console.error(error);
+        if (!selectedBarangId || !jumlah || parseInt(jumlah) <= 0) {
+          Swal.showValidationMessage('Harap pilih barang dan masukkan jumlah yang valid!');
+          return false; // Tidak melanjutkan proses jika data tidak valid
         }
-      );
-    }
-  });
 
-  
-    // Menambahkan fungsi filter untuk pencarian barang
-    const filterBarang = () => {
-      const searchValue = (document.getElementById('barangSelectInput') as HTMLInputElement).value.toLowerCase();
-      const selectElement = document.getElementById('barangSelect') as HTMLSelectElement;
-      const options = selectElement.options;
-  
-      // Menyaring opsi berdasarkan input pencarian
-      for (let i = 0; i < options.length; i++) {
-        const optionText = options[i].text.toLowerCase();
-        if (optionText.includes(searchValue)) {
-          options[i].style.display = ''; // Menampilkan item yang cocok
-        } else {
-          options[i].style.display = 'none'; // Menyembunyikan item yang tidak cocok
-        }
+        return { selectedBarangId, jumlah: parseInt(jumlah), keterangan };
       }
-    };
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const { selectedBarangId, jumlah, keterangan } = result.value;
+        // Menambahkan barang masuk melalui transaksiService
+        this.transaksiService.tambahBarangMasuk(selectedBarangId, jumlah, keterangan).subscribe(
+          (response) => {
+            Swal.fire({
+              title: 'Berhasil!',
+              text: 'Barang masuk berhasil ditambahkan.',
+              icon: 'success',
+              confirmButtonText: 'OK'
+            }).then(() => {
+              // Refresh halaman untuk memuat data baru
+              window.location.reload();
+            });
+          },
+          (error) => {
+            Swal.fire('Gagal!', 'Terjadi kesalahan saat menambahkan barang masuk.', 'error');
+            console.error(error);
+          }
+        );
+      }
+    });
   }
+
+
   
   
   
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,203 +1,181 @@
-import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { BarangService } from '../services/barang.service';
-import { TransaksiService } from '../services/transaksi.service';
-import { Barang } from '../models/barang.model';
-import { Transaksi } from '../models/transaksi.model';
-import Swal from 'sweetalert2';
-import { Subscription } from 'rxjs';
+import { environment } from '../environments/environment';
 
-@Component({
-  selector: 'app-barang-masuk',
-  standalone: false,
-  templateUrl: './barang-masuk.component.html',
-  styleUrls: ['./barang-masuk.component.css']
+@Injectable({
+  providedIn: 'root',
 })
-export class BarangMasukComponent implements OnInit, OnDestroy {
-  barangList: Barang[] = []; // Daftar barang
-  transaksiList: Transaksi[] = []; // Daftar transaksi barang masuk
-  filteredBarangMasukList: Transaksi[] = []; // Barang yang sudah difilter berdasarkan pencarian
-  selectedBarangId: string = ''; // ID barang yang dipilih
-  jumlah: number = 0; // Jumlah barang yang masuk
-  keterangan: string = ''; // Keterangan barang masuk
-  searchQuery: string = ''; // Query pencarian untuk barang masuk
-  p: number = 1; // Halaman untuk pagination (default 1)
+export class AuthService {
+  private url: string = environment.api + 'users/';
+  private authStatusListener = new Subject<boolean>();
 
-  private getBarangSub: Subscription = new Subscription();
-  private getTransaksiSub: Subscription = new Subscription();
+  private isAuthenticated = true;
+  private token: string | null = '';
+  private tokenTimer: any;
+
+  private isBrowser!: boolean;
+
+  loginListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   constructor(
-    private barangService: BarangService,
-    private transaksiService: TransaksiService,
-    @Inject(PLATFORM_ID) private platformId: any
-  ) {}
-
-  ngOnInit(): void {
-    this.loadBarangList(); // Memuat daftar barang pada saat komponen dimuat
-    this.loadTransaksiList(); // Memuat transaksi barang masuk
+    @Inject(PLATFORM_ID) platformId: Object,
+    public http: HttpClient,
+    private router: Router
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
   }
 
-  ngOnDestroy(): void {
-    // Unsubscribe untuk mencegah memory leaks
-    this.getBarangSub.unsubscribe();
-    this.getTransaksiSub.unsubscribe();
-  }
+  login(email: string, password: string) {
+    const user: User = {
+      _id: null,
+      email: email,
+      password: password,
+    };
 
-  // Memuat Daftar Barang dari BarangService
-  loadBarangList(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // Periksa apakah data barang sudah ada di localStorage
-      const storedBarangList = localStorage.getItem('barangList');
-      if (storedBarangList) {
-        this.barangList = JSON.parse(storedBarangList);
-      } else {
-        // Jika tidak ada, ambil data dari API
-        this.getBarangSub = this.barangService.getBarangListener().subscribe(
-          (data) => {
-            this.barangList = data;  // Menyimpan data barang yang diterima
-            localStorage.setItem('barangList', JSON.stringify(data));  // Simpan ke localStorage
-          },
-          (error) => {
-            console.error('Error loading barang list!', error);
+    console.log('User Login:', user);
+
+    this.http
+      .post<{ token: string; expiresIn: number }>(this.url + 'login', user)
+      .subscribe(
+        (response) => {
+          console.log('Backend Response:', response);
+
+          const token = response.token;
+          const expiresInDuration = response.expiresIn;
+
+          if (!expiresInDuration) {
+            console.error('Invalid expiration duration:', expiresInDuration);
+            return; // If expiresIn is undefined or invalid, stop further processing
           }
-        );
-        this.barangService.getBarang(); // Panggil API untuk mengambil data barang
-      }
-    } else {
-      console.warn('localStorage is not available in this environment');
-    }
-  }
 
-  // Memuat Daftar Transaksi Barang Masuk
-  loadTransaksiList(): void {
-    this.getTransaksiSub = this.transaksiService.getBarangMasuk().subscribe(
-      (data: Transaksi[]) => {
-        this.transaksiList = data;  // Menyimpan data transaksi yang diterima
-        this.filteredBarangMasukList = this.transaksiList; // Menampilkan seluruh transaksi sebelum pencarian
-      },
-      (error) => {
-        console.error('Error loading transaksi list!', error);
-      }
-    );
-  }
+          this.token = token;
+          if (token) {
+            // Check if expiresInDuration is valid
+            if (isNaN(expiresInDuration) || expiresInDuration <= 0) {
+              console.error('Invalid expiration duration:', expiresInDuration);
+              return; // Stop if expiresIn is not valid
+            }
 
-  // Fungsi untuk mencari barang
-  searchBarangMasuk(): void {
-    if (this.searchQuery.trim()) {
-      const query = this.searchQuery.toLowerCase();
-  
-      this.filteredBarangMasukList = this.transaksiList.filter((transaksi) => {
-        // Mengonversi tanggalMasuk ke format YYYY-MM-DD
-        const tanggalMasuk = new Date(transaksi.tanggalMasuk);
-        const formattedTanggalMasuk = tanggalMasuk instanceof Date && !isNaN(tanggalMasuk.getTime())
-          ? tanggalMasuk.toISOString().split('T')[0] // Format sebagai YYYY-MM-DD
-          : ''; // Jika tanggal tidak valid, berikan string kosong
-  
-        return (
-          transaksi.barang.nama.toLowerCase().includes(query) ||
-          transaksi.barang.merk.toLowerCase().includes(query) ||
-          transaksi.barang.jenis.toLowerCase().includes(query) ||
-          formattedTanggalMasuk.includes(query) || // Pencarian berdasarkan tanggal
-          (transaksi.keterangan && transaksi.keterangan.toLowerCase().includes(query)) // Pencarian berdasarkan keterangan
-        );
-      });
-    } else {
-      this.filteredBarangMasukList = this.transaksiList; // Tampilkan seluruh transaksi jika pencarian kosong
-    }
-  }
+            this.setAuthTimer(expiresInDuration);
+            this.isAuthenticated = true;
+            this.authStatusListener.next(true);
 
-  // Fungsi untuk menambah barang masuk
-  tambahBarangMasuk(): void {
-    if (this.selectedBarangId && this.jumlah > 0) {
-      this.transaksiService.tambahBarangMasuk(this.selectedBarangId, this.jumlah, this.keterangan).subscribe(
-        result => {
-          console.log('Barang masuk berhasil ditambahkan!', result);
-          // Setelah barang berhasil ditambahkan, muat ulang daftar transaksi
-          this.loadTransaksiList();
-        },
-        error => {
-          console.error('Error adding barang masuk!', error);
-        }
-      );
-    } else {
-      alert('Silakan pilih barang dan masukkan jumlah!');
-    }
-  }
+            const now = new Date();
+            const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
 
-  tampilkanFormTambah(): void {
-    // Menampilkan modal SweetAlert2 dengan form input pencarian barang
-    Swal.fire({
-      title: 'Tambah Barang Masuk',
-      html: `
-        <div class="form-group mb-3">
-          <!-- Select untuk memilih barang -->
-          <select id="barangSelect" class="form-control" style="margin-top: 10px;">
-            <option value="">-- Pilih Barang --</option>
-            ${this.barangList.map(barang => `
-              <option value="${barang._id}">${barang.nama} - ${barang.merk}</option>
-            `).join('')}
-          </select>
-        </div>
-        <div class="form-group mb-3">
-          <input type="number" id="jumlahInput" class="form-control" min="1" placeholder="Jumlah barang">
-        </div>
-        <div class="form-group">
-          <input type="text" id="keteranganInput" class="form-control" placeholder="Masukkan keterangan">
-        </div>
-      `,
-      showCancelButton: true,
-      confirmButtonText: 'Tambah',
-      cancelButtonText: 'Batal',
-      preConfirm: () => {
-        const selectedBarangId = (document.getElementById('barangSelect') as HTMLSelectElement).value;
-        const jumlah = (document.getElementById('jumlahInput') as HTMLInputElement).value;
-        const keterangan = (document.getElementById('keteranganInput') as HTMLInputElement).value;
+            // Check if expirationDate is valid
+            if (isNaN(expirationDate.getTime())) {
+              console.error('Invalid expiration date:', expirationDate);
+              return; // Stop if expirationDate is not valid
+            }
 
-        if (!selectedBarangId || !jumlah || parseInt(jumlah) <= 0) {
-          Swal.showValidationMessage('Harap pilih barang dan masukkan jumlah yang valid!');
-          return false; // Tidak melanjutkan proses jika data tidak valid
-        }
-
-        return { selectedBarangId, jumlah: parseInt(jumlah), keterangan };
-      }
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const { selectedBarangId, jumlah, keterangan } = result.value;
-        // Menambahkan barang masuk melalui transaksiService
-        this.transaksiService.tambahBarangMasuk(selectedBarangId, jumlah, keterangan).subscribe(
-          (response) => {
-            Swal.fire({
-              title: 'Berhasil!',
-              text: 'Barang masuk berhasil ditambahkan.',
-              icon: 'success',
-              confirmButtonText: 'OK'
-            }).then(() => {
-              // Refresh halaman untuk memuat data baru
+            console.log('Token expires at:', expirationDate);
+            this.saveAuthData(token, expirationDate);
+            this.router.navigate(['/admin/']).then(() => {
               window.location.reload();
             });
-          },
-          (error) => {
-            Swal.fire('Gagal!', 'Terjadi kesalahan saat menambahkan barang masuk.', 'error');
-            console.error(error);
           }
-        );
-      }
-    });
+        },
+        (error) => {
+          console.error('Login Error:', error);
+          this.authStatusListener.next(false);
+        }
+      );
+}
+
+
+  getToken() {
+    return this.token;
   }
 
-
-  
-  
-  
-  
-
-  // Fungsi untuk mengoptimalkan render dengan trackBy
-  trackById(index: number, item: any): string {
-    return item._id; // Menggunakan _id dari transaksi untuk trackBy
+  getIsAuth() {
+    console.log('AuthService - isAuthenticated:', this.isAuthenticated);
+    return this.isAuthenticated;
   }
 
-  // Fungsi untuk pagination
-  paginate(pageNumber: number): void {
-    this.p = pageNumber; // Menetapkan halaman yang dipilih
+  private saveAuthData(token: string, expirationDate: Date) {
+    if (isNaN(expirationDate.getTime())) {
+      console.error('Invalid expiration date:', expirationDate);
+      return; // Tidak menyimpan jika expirationDate tidak valid
+    }
+    console.log('Saving Auth Data:', { token, expirationDate });
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    let token = null;
+    let expirationDate = null;
+
+    if (this.isBrowser) {
+      token = localStorage.getItem('token');
+      expirationDate = localStorage.getItem('expiration');
+    }
+
+    if (!token || !expirationDate) {
+      return;
+    }
+
+    const parsedExpirationDate = new Date(expirationDate);
+    
+    // Memastikan expirationDate valid
+    if (isNaN(parsedExpirationDate.getTime())) {
+      console.error('Invalid expiration date:', parsedExpirationDate);
+      return;
+    }
+
+    return {
+      token: token,
+      expirationDate: parsedExpirationDate,
+    };
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+  
+    if (!authInformation) {
+      return;
+    }
+  
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+  
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+      this.router.navigate(['/admin/']);
+    }
+  }
+  
+
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
+    this.clearAuthData();
+    this.router.navigate(['/login']);
+  }
+
+  private setAuthTimer(duration: number) {
+    console.log('Setting timer: ' + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
   }
 }
